480b110959501aceba19022b065780d66e078f46
shlomyerba <shlomy71@gmail.com>
2025-06-25 11:06:56 +0300

    test2

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/example', exampleRouter);
-app.use('/api/example2', exampleRouter);
 app.use('/api/users', userRouter);
 
 export default app;
