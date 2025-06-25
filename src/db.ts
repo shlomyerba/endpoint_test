095947a5258aff42af608d230b74e20d694ca2ae
shlomyerba <shlomy71@gmail.com>
2025-06-26 00:54:30 +0300

    2

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,15 @@
 import { Sequelize } from 'sequelize';
 
+console.log("DB_NAME:", process.env.DB_NAME, "DB_USER=", process.env.DB_USER, "DB_PASSWORD=", process.env.DB_PASSWORD);
+
 export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
-    dialect: 'mysql',
-    logging: false,
-  }
+    process.env.DB_NAME || '',
+    process.env.DB_USER || '',
+    process.env.DB_PASSWORD || '',
+    {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT) || 3306,
+        dialect: 'mysql',
+        logging: false,
+    }
 );
