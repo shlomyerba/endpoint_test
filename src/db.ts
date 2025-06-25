74b359066f20f8b3513ff843f42503589b92f455
shlomyerba <shlomy71@gmail.com>
2025-06-25 12:23:58 +0300

    db connection to env

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,16 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize('endpoint_dev', 'root', '7183998a', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
